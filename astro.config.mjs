f6647a1ab2006e2cbba9f9423acbf94314b8e245
TWProbe <info.twprobe@gmail.com>
2026-04-07 22:15:52 +0800

    fix: inject site URL to generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  // 注入絕對物理座標 (Sitemap 引擎的必須燃料)
+  site: 'https://hub.twprobe.com',
+
+  integrations: [
+    tailwind(),
+    sitemap()
+  ],
+
+  // 啟動物理預載引擎
+  prefetch: true
 });
